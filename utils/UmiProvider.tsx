eb735bb894e403da5b6ae5e51529e46f183a07ff
BohemiaNFT <156927413+bohemiaNFT@users.noreply.github.com>
2024-09-23 00:49:32 +0200

    a

--- utils/UmiProvider.tsx
+++ utils/UmiProvider.tsx
@@ -1,13 +1,12 @@
-import { createUmi } from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
 import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { ReactNode } from "react";
 import { UmiContext } from "./useUmi";
-import { mplCandyMachine } from "@metaplex-foundation/mpl-core-candy-machine";
+import { mplCandyMachine } from  "@metaplex-foundation/mpl-core-candy-machine"
 import { createNoopSigner, publicKey, signerIdentity } from "@metaplex-foundation/umi";
 import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
-import { web3JsRpc } from "@metaplex-foundation/umi-rpc-web3js"; // Correct plugin for RPC
 
 export const UmiProvider = ({
   endpoint,
@@ -17,17 +16,15 @@ export const UmiProvider = ({
   children: ReactNode;
 }) => {
   const wallet = useWallet();
-  const umi = createUmi()
-    .use(web3JsRpc(endpoint)) // Use the web3JsRpc plugin to set the endpoint
-    .use(mplTokenMetadata())
-    .use(mplCandyMachine()) // Ensure correct plugin usage
-    .use(dasApi());
-
+  const umi = createUmi(endpoint)
+  .use(mplTokenMetadata())
+  .use(mplCandyMachine())
+  .use(dasApi())
   if (wallet.publicKey === null) {
-    const noopSigner = createNoopSigner(publicKey("11111111111111111111111111111111"));
+    const noopSigner = createNoopSigner(publicKey("11111111111111111111111111111111"))
     umi.use(signerIdentity(noopSigner));
   } else {
-    umi.use(walletAdapterIdentity(wallet));
+    umi.use(walletAdapterIdentity(wallet))
   }
 
   return <UmiContext.Provider value={{ umi }}>{children}</UmiContext.Provider>;
