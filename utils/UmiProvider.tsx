b274376ef0079c949fc2d64c8b82b7540e2ec92c
BohemiaNFT <156927413+bohemiaNFT@users.noreply.github.com>
2024-09-22 23:13:11 +0200

    Update UmiProvider.tsx

--- utils/UmiProvider.tsx
+++ utils/UmiProvider.tsx
@@ -7,6 +7,7 @@ import { UmiContext } from "./useUmi";
 import { mplCandyMachine } from  "@metaplex-foundation/mpl-core-candy-machine"
 import { createNoopSigner, publicKey, signerIdentity } from "@metaplex-foundation/umi";
 import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
+import { coreGuards } from '@metaplex-foundation/umi-core-guards'; // Ensure this import
 
 export const UmiProvider = ({
   endpoint,
@@ -20,6 +21,7 @@ export const UmiProvider = ({
   .use(mplTokenMetadata())
   .use(mplCandyMachine())
   .use(dasApi())
+  .use(coreGuards()) // Ensure this usage
   if (wallet.publicKey === null) {
     const noopSigner = createNoopSigner(publicKey("11111111111111111111111111111111"))
     umi.use(signerIdentity(noopSigner));
