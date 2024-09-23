f40eed4ef7f4e5d1a56dec8ff39d8cb4fb08d3de
BohemiaNFT <156927413+bohemiaNFT@users.noreply.github.com>
2024-09-23 12:39:56 +0200

    Update UmiProvider.tsx

--- utils/UmiProvider.tsx
+++ utils/UmiProvider.tsx
@@ -1,20 +1,12 @@
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-// Removed import of GuardRepository
 import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
 import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { ReactNode } from "react";
 import { UmiContext } from "./useUmi";
-import { mplCandyMachine } from "@metaplex-foundation/mpl-core-candy-machine";
+import { mplCandyMachine } from  "@metaplex-foundation/mpl-core-candy-machine"
 import { createNoopSigner, publicKey, signerIdentity } from "@metaplex-foundation/umi";
 import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
-
-// Define a custom type that extends the Umi instance
-interface CustomUmi {
-  coreGuards?: any; // Updated type to any or appropriate type
-  use: (plugin: any) => this;
-  // Add other properties and methods as needed
-}
 
 export const UmiProvider = ({
   endpoint,
@@ -24,20 +16,15 @@ export const UmiProvider = ({
   children: ReactNode;
 }) => {
   const wallet = useWallet();
-  const umi = createUmi(endpoint) as CustomUmi;
-
-  umi.use(mplTokenMetadata())
-    .use(mplCandyMachine())
-    .use(dasApi());
-
-  // Ensure coreGuards is included if required
-  umi.coreGuards = umi.coreGuards || {}; // Initialize coreGuards if it's required and missing
-
+  const umi = createUmi(endpoint)
+  .use(mplTokenMetadata())
+  .use(mplCandyMachine())
+  .use(dasApi())
   if (wallet.publicKey === null) {
-    const noopSigner = createNoopSigner(publicKey("11111111111111111111111111111111"));
+    const noopSigner = createNoopSigner(publicKey("11111111111111111111111111111111"))
     umi.use(signerIdentity(noopSigner));
   } else {
-    umi.use(walletAdapterIdentity(wallet));
+    umi.use(walletAdapterIdentity(wallet))
   }
 
   return <UmiContext.Provider value={{ umi }}>{children}</UmiContext.Provider>;
